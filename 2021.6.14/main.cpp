871141f52ffe6de64cb8244c824a9b86ff22dda7
norbinorbert <47588133+norbinorbert@users.noreply.github.com>
2021-06-14 07:26:47 +0300

    Add files via upload

---
+++ 2021.6.14/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+struct node{
+    int index;
+    node * bal;
+    node * jobb;
+};
+
+struct node*letrehoz(int index){
+    node * elem = new node;
+    elem->bal = NULL;
+    elem->jobb = NULL;
+    elem->index = index;
+    return elem;
+};
+
+void felepit(struct node*current){
+    int csomo;
+    cout << "Kerem az " << current->index << " elem bal oldali gyereket (0 ha nincs)";
+    cin >> csomo;
+    if(csomo!=0){
+        node*uj = letrehoz(csomo);
+        current->bal = uj;
+        felepit(current->bal);
+    }
+    cout << "Kerem az " << current->index << " elem jobb oldali gyereket (0 ha nincs)";
+    cin >> csomo;
+    if(csomo!=0){
+        node*uj = letrehoz(csomo);
+        current->jobb = uj;
+        felepit(current->jobb);
+    }
+}
+void bejar(struct node*current){
+    if(current->bal != NULL){
+        bejar(current->bal);
+    }
+    cout << current->index << " ";
+    if(current->jobb != NULL){
+        bejar(current->jobb);
+    }
+}
+
+int main()
+{
+    node*first = letrehoz(1);
+    node*current = first;
+    felepit(first);
+    bejar(first);
+    return 0;
+}
